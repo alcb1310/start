58eecfba512a12676ecce3a49e14320b6d82c517
Andres Court <andres@andrescourt.com>
2025-04-05 20:10:54 -0500

    feat(router): router

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { createRouter as createTanstackRouter } from "@tanstack/react-router";
+// @ts-expect-error
+import { routeTree } from './routeTree.gen'
+
+export function createRouter() {
+  const router = createTanstackRouter({
+    routeTree,
+    scrollRestoration: true
+  })
+
+  return router
+}
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>
+  }
+}
