1248c07a957c505c0c0e08107fb6b06f60893175
Andres Court <andres@andrescourt.com>
2025-04-06 13:13:50 -0500

    feat(posts): get all posts from database

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,9 +1,38 @@
+import { Button } from '@/components/ui/button'
+import { PrismaClient } from '@prisma/client'
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+
+const prisma = new PrismaClient()
 
 export const Route = createFileRoute('/posts/')({
 	component: RouteComponent,
+	loader: async () => {
+		const posts = await getPosts()
+
+		return {
+			posts,
+		}
+	},
 })
 
 function RouteComponent() {
-	return <div>Hello "/posts/"!</div>
+	const { posts } = Route.useLoaderData()
+
+	return (
+		<>
+			<Button>New Post</Button>
+			{!posts.length && <p>No posts yet</p>}
+
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</>
+	)
 }
+
+const getPosts = createServerFn().handler(async () => {
+	return prisma.post.findMany()
+})
